bf15f3bb557c78c468f279bc7835a31659120577
ymaretsky <75186304+ymaretsky@users.noreply.github.com>
2021-01-31 22:39:17 +0300

    Create TestHello.java

---
+++ src/TestHello.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+public class TestHello {
+    @Test
+    public void testHello() {
+        HelloWorld hello = new HelloWorld();
+        assertEquals("Hello, World!", hello.sayHello());
+    }
+}
