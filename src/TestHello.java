b3b969c852274ff0a33f620a1d1810c3e18e8909
ymaretsky <75186304+ymaretsky@users.noreply.github.com>
2021-02-04 00:23:06 +0300

    Delete TestHello.java

--- src/TestHello.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
-public class TestHello {
-    @Test
-    public void testHello() {
-        HelloWorld hello = new HelloWorld();
-        assertEquals("Hello, World!", hello.sayHello());
-    }
-}
