cbbc3c0e26a66ba66b8cfa296e85e48e1dc152f3
ymaretsky <75186304+ymaretsky@users.noreply.github.com>
2021-02-07 19:30:33 +0300

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,19 +1,6 @@
-import javax.swing.*;
-import java.awt.*;
-// public class HelloWorld extends JFrame {
-    public static void main(String[] args) {
-        new HelloWorld();
-    }
-    HelloWorld() {
-        setTitle(sayHello());
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setBounds(200, 200, 300, 200);
-        JLabel label = new JLabel(sayHello(), SwingConstants.CENTER);
-        label.setFont(new Font("", Font.BOLD, 24));
-        add(label);
-        setVisible(true);
-    }
-    String sayHello() {
-        return "Hello, World!";
-    }
-// }
+<?xml version="1.0"?>
+<project name="HelloWorld" default="hello">
+    <target name="hello">
+        <echo>Hello, World!</echo>
+    </target>
+</project>
